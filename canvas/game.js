61130a3c2978f5f9acb1a5d5583b724fd84c5777
Marks <nxmarks@labs.neit.edu>
2018-09-10 10:44:11 -0400

    new color change mechanic

--- canvas/game.js
+++ canvas/game.js
@@ -10,9 +10,20 @@ s.vx = 5;
 s.vy = 9;
 
 
+var ballColor = ["red", "blue", "yellow", "gray", "black", "green", "orange", "purple"];
+
+function rand(low, high)
+{
+    return Math.random()*(high - low) + low;
+}
+
 function main()
 {
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    var newColor = ballColor[Math.round(rand(0,7))];
+    console.log(newColor);
+
+    s.color = newColor;
 
     if(s.x >= canvas.width - s.w)
     {
