cbcf0033f094693640bd40d25470501db194c29d
Marks <nxmarks@labs.neit.edu>
2018-09-10 10:54:24 -0400

    random movement added

--- canvas/game.js
+++ canvas/game.js
@@ -4,10 +4,7 @@ var ctx = canvas.getContext("2d");
 var timer = setInterval(main, 1000/60);
 
 
-var s = new GameObject();
-s.color = "red";
-s.vx = 5;
-s.vy = 9;
+
 
 
 var ballColor = ["red", "blue", "yellow", "gray", "black", "green", "orange", "purple"];
@@ -16,6 +13,11 @@ function rand(low, high)
 {
     return Math.random()*(high - low) + low;
 }
+
+var s = new GameObject();
+s.color = "red";
+s.vx = Math.round(rand(-10,10));
+s.vy = Math.round(rand(-10,10));
 
 function main()
 {
@@ -27,19 +29,19 @@ function main()
 
     if(s.x >= canvas.width - s.w)
     {
-        s.vx = -5;
+        s.vx = Math.round(rand(-10,-1));
     }
     if(s.x <= 0)
     {
-        s.vx = 5;
+        s.vx = Math.round(rand(1,10));
     }
     if(s.y >= canvas.height - s.h)
     {
-        s.vy = -9;
+        s.vy = Math.round(rand(-10,-1));
     }
     if(s.y <= 0)
     {
-        s.vy = 9;
+        s.vy = Math.round(rand(1,10));
     }
     s.move();
     s.draw();
